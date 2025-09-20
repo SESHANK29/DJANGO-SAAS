71c494116778a913e46865688353716c2e5cf3f0
SESHANK29 <sesanksaibadisa029@gmail.com>
2025-09-20 16:34:32 +0530

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -298,8 +298,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'myproject',
+            'USER': 'postgres',
+            'PASSWORD': 'Seshank1289',
+            'HOST': 'localhost',
+            'PORT': '5432',
         },
         'OPTIONS': {
             "timeout": 20,
